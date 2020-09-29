6c67f4ab8d481288ae4f1534c07a9700e94df056
Rubens Bispo <rubensbispo29@gmail.com>
2020-09-29 20:31:32 -0300

    Inicio do projeto

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
-<?php
+<?php 
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
+require_once("vendor/autoload.php");
 
-require "./vendor/autoload.php";
+$app = new \Slim\Slim();
 
-$app = AppFactory::create();
+$app->config('debug', true);
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+$app->get('/', function() {
+    
+	echo "OK";
+
 });
 
 $app->run();
+
+ ?>
