a8c95c82cc170d4711af8682759b8ce0e5320af8
Rubens Bispo <rubensbispo29@gmail.com>
2020-10-04 15:02:49 -0300

    Testando ambiente

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
-<?php 
-
-require_once("vendor/autoload.php");
-
-$app = new \Slim\Slim();
-
-$app->config('debug', true);
-
-$app->get('/', function() {
+<?php
+	
+  require_once("vendor/autoload.php");
+  
+  $app = new \Slim\Slim();
+  
+  $app->config('debug', true);
+  
+  $app->get('/', function() 
+  {
+    try 
+    {
+      $sql = new \Hcode\DB\Sql();
+      if ($sql)
+      {
+	$results = $sql->select("select * from tb_users");
+	echo json_encode($results);
+      }
+    }
+    catch (Exception $e)
+    {
+      print_r($e->getMessage());
+    }
     
-	echo "OK";
-
 });
-
-$app->run();
-
- ?>
+  $app->run();
+?> 
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit2a6c5b4a233a06c7eab6560771a9c9b9
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit2a6c5b4a233a06c7eab6560771a9c9b9
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit2a6c5b4a233a06c7eab6560771a9c9b9::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit2a6c5b4a233a06c7eab6560771a9c9b9::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit2a6c5b4a233a06c7eab6560771a9c9b9::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit2a6c5b4a233a06c7eab6560771a9c9b9::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit2a6c5b4a233a06c7eab6560771a9c9b9::$classMap;
 
